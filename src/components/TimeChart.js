bb6a9e3b38430481ecf8ad0b4e845674a56f1c66
CoryMci <corymciver@live.ca>
2023-02-22 15:13:20 -0800

    Style changes to timechart component

--- src/components/TimeChart.js
+++ src/components/TimeChart.js
@@ -12,6 +12,7 @@ import {
   Tooltip,
 } from "chart.js";
 import { useState } from "react";
+import parseTime from "../helpers/parseTime";
 
 function randomColor() {
   const letters = "0123456789ABCDEF";
@@ -45,6 +46,7 @@ export default function TimeChart(props) {
   });
 
   const taskData = {};
+  let TotalTime = 0;
   const dates = [];
   for (let i = dateRange; i >= 0; i--) {
     dates.push(moment().subtract(i, "days").format("YYYY-MM-DD"));
@@ -73,6 +75,7 @@ export default function TimeChart(props) {
       const index = dates.indexOf(date);
       if (index !== -1) {
         task.data[index] += log.duration;
+        TotalTime += log.duration;
       }
     }
   });
@@ -98,73 +101,87 @@ export default function TimeChart(props) {
         onClick={handleCloseChart}
         className="fixed z-40 w-screen h-screen bg-black bg-opacity-30"
       >
-        <div className="absolute rounded-xl bg-white w-1/4 h-min mx-auto inset-x-0 inset-y-1/4 p-4">
+        <div
+          onClick={(e) => e.stopPropagation()}
+          className="grid grid-flow-row gap-4 absolute rounded-xl bg-white w-1/4 h-min mx-auto inset-x-0 inset-y-1/4 p-4"
+        >
+          <h2 className="text-xl font-medium mb-4">Report</h2>
           <button
             onClick={handleCloseChart}
             className="material-symbols-outlined absolute top-2 right-2 font-bold text-gray-400 text-l"
           >
             close
           </button>
-          <div className="bg-red-500 w-max rounded-md">
+          <div className="border border-red-400 w-max rounded-md overflow-hidden">
             <button
-              className="font-bold text-gray-500 text-l mx-3"
+              className={`font-bold  text-l border-r px-3 ${
+                dateRange == 6 ? "bg-red-400 text-white" : "text-red-400"
+              }`}
               onClick={(e) => handleDateRangeChange(e, 6)}
             >
-              Last week
+              Past week
             </button>
 
             <button
-              className="font-bold text-gray-500 text-l mx-3"
+              className={`font-boldtext-l px-3 ${
+                dateRange == 6 ? "text-red-400" : "bg-red-400 text-white"
+              }`}
               onClick={(e) => handleDateRangeChange(e, 30)}
             >
-              Last month
+              Past month
             </button>
           </div>
+          <div>
+            <div className="text-gray-500 uppercase tracking-tight text-xs m-2">
+              Total time this {dateRange == 6 ? "Week" : "Month"}
+            </div>
+            <div className="tracking-tight text-3xl m-2">
+              {parseTime(TotalTime)}
+            </div>
+          </div>
 
-          <Bar
-            data={chartData}
-            options={{
-              scales: {
-                x: {
-                  stacked: true,
-                },
-                y: {
-                  stacked: true,
-                  ticks: {
-                    callback: function (value) {
-                      // Convert the tick value from seconds to hours and format it as "hh:mm"
-                      const hours = Math.floor(value / 3600);
-                      const minutes = Math.floor((value % 3600) / 60);
-                      return `${hours.toString().padStart(2, "0")}:${minutes
-                        .toString()
-                        .padStart(2, "0")}`;
+          <div className="w-full h-auto border-t-2 font-bold">
+            <Bar
+              data={chartData}
+              options={{
+                scales: {
+                  x: {
+                    stacked: true,
+                  },
+                  y: {
+                    stacked: true,
+                    ticks: {
+                      callback: function (value) {
+                        // Convert the tick value from seconds to hours and format it as "hh:mm". Different method than parseTime lib because of chart functionality
+                        const hours = Math.floor(value / 3600);
+                        const minutes = Math.floor((value % 3600) / 60);
+                        return `${hours.toString().padStart(2, "0")}:${minutes
+                          .toString()
+                          .padStart(2, "0")}`;
+                      },
                     },
                   },
                 },
-              },
-              plugins: {
-                title: {
-                  display: true,
-                  text: "Task Time Chart",
-                },
-                tooltip: {
-                  callbacks: {
-                    label: function (context) {
-                      return context.dataset.label;
-                    },
-                    title: function (context) {
-                      return (
-                        moment
-                          .duration(context[0].parsed.y, "seconds")
-                          .asHours()
-                          .toFixed(2) + " hrs"
-                      );
+                plugins: {
+                  tooltip: {
+                    callbacks: {
+                      label: function (context) {
+                        return context.dataset.label;
+                      },
+                      title: function (context) {
+                        return (
+                          moment
+                            .duration(context[0].parsed.y, "seconds")
+                            .asHours()
+                            .toFixed(2) + " hrs"
+                        );
+                      },
                     },
                   },
                 },
-              },
-            }}
-          />
+              }}
+            />
+          </div>
         </div>
       </div>
     )
